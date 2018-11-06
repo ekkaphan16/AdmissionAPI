5581c7130f00f1a09786680381fd6cdc25b5cd50
Ekkaphan Kh <ekkaphan.kh@gmail.com>
2018-11-06 09:14:54 +0700

    Update dbconnection.js

--- dbconnection.js
+++ dbconnection.js
@@ -1,10 +1,10 @@
 var mysql=require('mysql');
 var connection=mysql.createPool({
 
-host:'202.29.22.75',
-user:'tcas_user',
-password:'NoThePass',
-database:'tcas_db'
+host:'',
+user:'',
+password:'',
+database:''
 
 
 });
